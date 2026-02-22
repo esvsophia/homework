caa202ed590290eba3b7a9b4d4d217ab50f84fd0
sofiaegorova <sondanu@yandex.ru>
2026-02-22 14:26:55 +0300

    rename file

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String change = "Я настроила GitHub";
+        System.out.println("Моя строка: " + change);
+    }
+}
--- main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public void static main {
-        String change = 'Я настроила GitHub';
-        System.out.println('Моя строка: ' + change);
-    }
-}
