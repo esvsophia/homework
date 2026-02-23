c735dd01011d6c639960255e9bb1c914349be95f
sofiaegorova <sondanu@yandex.ru>
2026-02-23 17:56:10 +0300

    Изменила строку в новой ветке

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String change = "Я настроила GitHub";
+        String change = "Изменение новой ветки";
         System.out.println("Моя строка: " + change);
     }
 }
