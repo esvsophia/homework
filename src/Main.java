8f7877e05bffe048291a0c274623b037ca997555
sofiaegorova <sondanu@yandex.ru>
2026-02-23 17:39:15 +0300

    add my phrase

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String change = "Я сделала дз";
+        String change = "Я настроила GitHub";
         System.out.println("Моя строка: " + change);
     }
 }
