6105cb20ca6827cc4ef7bec2e414f098167f98b7
sofiaegorova <sondanu@yandex.ru>
2026-02-23 18:47:38 +0300

    change phrase by sofa part 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String change = "Я настроила GitHub";
+        String change = "Я настроила GitHub и все работает";
         System.out.println("Моя строка: " + change);
     }
 }
