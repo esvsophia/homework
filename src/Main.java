39b71b5027f38eedc4a003f10f0dd8d31ff72145
Yartseva2508 <iyartseva01@yandex.ru>
2026-02-23 18:45:46 +0300

    changes of srting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String change = "Я сделала дз";
+        String change = "Я сделала домашнее задание";
         System.out.println("Моя строка: " + change);
     }
 }
