d729b5cc7c8596ead64bb328b1eb8f3b8399feb2
sofiaegorova <sondanu@yandex.ru>
2026-02-23 17:25:38 +0300

    almost final changes of sofa

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String change = "Я настроила GitHub";
+        String change = "Я настроила GitHu";
         System.out.println("Моя строка: " + change);
     }
 }
